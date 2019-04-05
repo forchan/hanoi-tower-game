36b7917e4493515b7078533d899cd249cc8816a8
forchan <conloonc@buffalo.edu>
2019-04-05 14:18:30 -0400

    Updates

---
+++ precache-manifest.0c1313b5e561d4306fd42c68be70a33d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hanoi-tower-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/hanoi-tower-game/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "4ad31f48aff04984678b",
+    "url": "/hanoi-tower-game/static/js/runtime~main.ae07d220.js"
+  },
+  {
+    "revision": "953dff6db097d51e20e8",
+    "url": "/hanoi-tower-game/static/js/main.892981dc.chunk.js"
+  },
+  {
+    "revision": "6fad19780250c28d9d70",
+    "url": "/hanoi-tower-game/static/js/2.ba45bef3.chunk.js"
+  },
+  {
+    "revision": "953dff6db097d51e20e8",
+    "url": "/hanoi-tower-game/static/css/main.482d9fa2.chunk.css"
+  },
+  {
+    "revision": "6fad19780250c28d9d70",
+    "url": "/hanoi-tower-game/static/css/2.0e792be9.chunk.css"
+  },
+  {
+    "revision": "e7ded4aabd12f978e004c3ec40782be4",
+    "url": "/hanoi-tower-game/index.html"
+  }
+];
