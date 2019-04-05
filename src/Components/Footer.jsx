98d768e14e3cf3061a20a29730a5b0094ae5445c
forchan <conloonc@buffalo.edu>
2019-04-04 21:48:11 -0400

    github logos and disk color change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import githubMark from '../styles/GitHub_Mark.png';
+import githubLogo from '../styles/GitHub_Logo.png';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const Footer = () => {
@@ -7,14 +9,14 @@ const Footer = () => {
       <NavbarBrand />
       <Nav className="ml-auto" navbar>
         <NavItem>
-          <NavLink href="https://github.com/forchan/hanoi-tower-game" target="_blank">
-            <span style={{ color: 'white' }}>
-              made by @chrischan on
-            </span>
-            {' '}
-            <span>
-              GitHub
-            </span>
+          <NavLink
+            className="github-link"
+            href="https://github.com/forchan/hanoi-tower-game"
+            target="_blank"
+          >
+            made by chrischan on{' '}
+            <img src={githubMark} alt="GitHub mark" style={{ height: '22px'}} />
+            <img src={githubLogo} alt="GitHub logo" style={{ height: '22px'}} />
           </NavLink>
         </NavItem>
       </Nav>
